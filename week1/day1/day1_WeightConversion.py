aa9cc0389c670d0feef570e4b9a2f714b2ed0152
Khaledkhb0 <khaledkhb2000.dz@gmail.com>
2026-06-16 21:11:52 +0200

    Restore day1 files

---
+++ week1/day1/day1_WeightConversion.py
@@ -0,0 +1,14 @@
+weight = float(input("Enter your weight: "))
+unit = input("Kilograms or Pounds? (K or L): ")
+
+if unit == "K" or unit =="k":
+    weight = weight * 2.205
+    unit = "Lbs."
+    print(f"Your weight is: {round(weight,2)} {unit}")
+elif unit == "L" or unit == "l":
+    weight = weight / 2.205
+    unit = "Kgs."
+    print(f"Your weight is: {round(weight,2)} {unit}")
+else:
+    print(f"{unit} was not valid")      
+
