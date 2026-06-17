3147d91461b7f7b7a961b387f64ea50fcfe813f4
Khaledkhb0 <khaledkhb2000.dz@gmail.com>
2026-06-17 22:51:53 +0200

    Day 1: string methods, indexing, format specifiers

--- week1/day1/day1_strings.py
+++ week1/day1/day1_strings.py
@@ -59,3 +59,119 @@ weather = "HOT" if temperature > 20 else "COLD"
 access_level = "Full Access" if user_role == "admin" else "Limited access"
 
 print(access_level)
+
+
+
+# =======================
+# STRING METHODS
+# =======================
+
+name = input("Enter your full name: ")
+
+result = len(name)
+result = name.find(" ")   #The index of the first occurrence
+result = name.rfind("q")  #The index of the last occurrence
+name = name.capitalize()  
+name = name.upper()       
+name = name.lower()
+result = name.isdigit()
+result = name.isalpha()
+
+phone_number = input("Enter your phone number: ")
+
+result = phone_number.count("-")
+phone_number = phone_number.replace("-"," ")
+
+
+print(result)
+print(phone_number)
+
+
+# EXERCISE: Validate user input exercise
+# 1. username is no more than 12 characters
+# 2. username must not contain spaces
+# 3. username must not contain digits
+
+username = input("Enter a username: ")
+
+if len(username) > 12:
+    print("Username INVALID ! Your username can't be more than 12 characters ")
+elif not username.find(" ") == -1 :
+    print("Username INVALID ! Your username can't contain spaces")
+elif not username.isalpha(): 
+    print("Username INVALID ! Your username can't contain digits")  
+else:
+    print(f"Welcome {username}")   
+
+
+
+# =======================
+# STRINGS INDEXING
+# =======================
+
+# [start : end : step]
+
+credit_number = "1234-5678-9012-3456"
+
+print(credit_number[0])
+print(credit_number[:4])
+print(credit_number[5:9])
+print(credit_number[5:])
+print(credit_number[-1])
+print(credit_number[::2])
+
+last_digits = credit_number[-4 : ]
+print(f"XXXX-XXXX-XXXX-{last_digits}")
+
+credit_number = credit_number[::-1]
+print(credit_number)
+
+
+
+# =======================
+# FORMAT SPECIFIERS
+# =======================
+
+# format specifiers = {value:flags} format a value based on what flags are inserted
+
+# .(number)f ---> round to that many decimal places (fixed point)
+# :(number)  ---> allocate that many spaces
+# :03        ---> allocate and zero pad that many spaces
+# :<         ---> left justify
+# :>         ---> right justify
+# :^         ---> center align
+# :+         ---> use a plus sign to indicate positive value
+# :=         ---> place sign to leftmost position
+# :          ---> insert a space before positive numbers
+# :,         ---> comma separator  
+
+
+price1 = 3000.14159
+price2 = -9807.65
+price3 = 1002.34
+
+print(f"Price 1 is {price1:.2f}")
+print(f"Price 2 is {price2:.2f}")
+print(f"Price 3 is {price3:.2f}")
+
+print(f"Price 1 is {price1:10}")
+
+print(f"Price 1 is {price1:010}")
+
+print(f"Price 1 is {price1:>10}")
+
+print(f"Price 1 is {price1:<10}")
+
+print(f"Price 1 is {price1:+}")
+print(f"Price 2 is {price2:+}")
+print(f"Price 3 is {price3:+}")
+
+print(f"Price 1 is {price1:,}")
+print(f"Price 2 is {price2:,}")
+print(f"Price 3 is {price3:,}")
+
+print(f"Price 1 is {price1:,.2f}")
+print(f"Price 2 is {price2:,.2f}")
+print(f"Price 3 is {price3:,.2f}")
+
+
