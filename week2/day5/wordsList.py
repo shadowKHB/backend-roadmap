0bff9e824156786f6b3f4c883b554731cfa122ee
Khaledkhb0 <khaledkhb2000.dz@gmail.com>
2026-07-13 10:51:23 +0200

    Day12 :words list for hangman game

---
+++ week2/day5/wordsList.py
@@ -0,0 +1,25 @@
+# Words for hangman game
+
+words = (
+    "Pelé", "Maradona", "Zidane", "Nazario", "Ronaldinho",
+    "Cruyff", "Beckenbauer", "Henry", "Maldini", "Iniesta",
+    "Hernandez", "Ibrahimovic", "Aguero", "Villa", "Carlos",
+    "Cafu", "Buffon", "Casillas", "Drogba", "Etoo",
+    "Kaka", "Totti", "Gerrard", "Lampard", "Giggs",
+    "Scholes", "Beckham", "Figo", "Pirlo", "Bale",
+    "Rooney", "ozil", "Fabregas", "Robben", "Ribery",
+    "Lahm", "Schweinsteiger", "Klose", "Puyol", "Pique",
+    "Zanetti", "Rivaldo", "Romario", "Batistuta", "Puskas",
+    "Eusebio", "Platini", "Best", "Gullit", "Hazard",
+    "Messi", "Ronaldo", "Mbappe", "Haaland", "Junior",
+    "Bellingham", "De Bruyne", "Salah", "Yamal", "Rodri",
+    "Kane", "Lewandowski", "Modric", "Neymar Jr", "Benzema",
+    "Griezmann", "Silva", "Fernandes", "Saka", "Foden",
+    "Musiala", "Wirtz", "Raphinha", "Dembele", "Valverde",
+    "Odegaard", "Rice", "van Dijk", "Dias", "Rudiger",
+    "Hakimi", "Hernandez", "Davies", "Marquinhos", "Saliba",
+    "Gavi", "Pedri", "Becker", "Courtois", "Ederson",
+    "Martinez", "Maignan", "Son", "Diaz", "Kvaratskhelia",
+    "Gyokeres", "Leao", "Alvarez", "Tchouameni"
+
+)
